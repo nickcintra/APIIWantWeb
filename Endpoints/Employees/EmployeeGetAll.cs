3dcb47a5626f4e65ed74166802680cd3d404a24c
Nicolas <nickcintra@hotmail.com>
2023-02-13 16:02:29 -0300

    primeira consulta utilizando Dapper

--- Endpoints/Employees/EmployeeGetAll.cs
+++ Endpoints/Employees/EmployeeGetAll.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Dapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Data.SqlClient;
 using System.Security.Claims;
 
 namespace iWantApp.Endpoints.Employees;
@@ -9,19 +11,18 @@ public class EmployeeGetAll
     public static string[] Methods => new string[] { HttpMethod.Get.ToString() };
     public static Delegate Handle => Action;
 
-    public static IResult Action(int page, int rows, UserManager<IdentityUser> userManager)
+    public static IResult Action(int? page, int? rows, IConfiguration configuration)
     {
+        var db = new SqlConnection(configuration["ConnectionString:IWantDb"]);
+        var query =
+                @"select Email, ClaimValue as Name
+                from AspNetUsers u inner
+                join AspNetUserClaims c
+                on u.id = c.UserId and claimtype = 'Name'
+                order by name
+                OFFSET (@page -1) * @rows ROWS FETCH NEXT @rows ROWS ONLY";
+        var employees = db.Query<EmployeeResponse>(query,new { page, rows });
 
-        var users = userManager.Users.Skip((page - 1) * rows).Take(rows).ToList();
-        var employees = new List<EmployeeResponse>();
-        foreach (var item in users)
-        {
-            var claims = userManager.GetClaimsAsync(item).Result;
-            var claimName = claims.FirstOrDefault(c => c.Type == "Name");
-            var userName = claimName != null ? claimName.Value : string.Empty;
-            employees.Add(new EmployeeResponse(item.Email, userName));
-        }
-        
         return Results.Ok(employees);
     }
 }
